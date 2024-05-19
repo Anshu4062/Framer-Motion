4397cc5e9501b7707e9584b02e63758d0688572b
Suryansh Singh <106943267+Anshu4062@users.noreply.github.com>
2024-05-19 11:46:02 +0530

    Add files via upload

---
+++ Pizza-point using Framer Motion/precache-manifest.e159bcccfa82896eec3c46642025ecdd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a09843f0682c383ee3ea91f5e1959f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b69ddf8b43f755fd4ed",
+    "url": "/static/css/main.a7393c36.chunk.css"
+  },
+  {
+    "revision": "15f75ffa9bb12fe165df",
+    "url": "/static/js/2.e523daa7.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.e523daa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b69ddf8b43f755fd4ed",
+    "url": "/static/js/main.bb0cae48.chunk.js"
+  },
+  {
+    "revision": "250e79f0bc1356899259",
+    "url": "/static/js/runtime-main.2e304d47.js"
+  },
+  {
+    "revision": "c7e91cd78db61f3efe1e483688b3275b",
+    "url": "/static/media/pizza4.c7e91cd7.jpg"
+  }
+]);
